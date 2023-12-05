1619c43886bdc7975ca0977fa9e7dfad0996346a
Yaqi Fang <cfang322@gmail.com>
2023-12-05 14:36:26 -0500

    Finished phase 1 twosum problem

--- practice-for-ch-js-intro-js-exercises-long-practice-main/practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
@@ -4,15 +4,31 @@
 Array.prototype.uniq = function() {
   let uniqueArr = [];
 
-
   for (let i = 0; i < this.length; i++) {
     if (!uniqueArr.includes(this[i])) {
       uniqueArr.push(this[i]);
     };
-
   };
   return uniqueArr;
 };
 
+// console.log([1,2,2,3,3,3].uniq())
 
-console.log([1,2,2,3,3,3].uniq())
+
+// return nested arr of indices of nums that add to 0
+Array.prototype.twoSum = function() {
+    let indexSum = [];
+
+    for (let i = 0; i < this.length; i++){
+        for (let j = i + 1; j < this.length; j++){
+            if (j > i) {
+                if (this[i] + this[j] === 0) {
+                        indexSum.push([i, j]);
+                };
+            };
+        };
+    };
+
+    return indexSum;
+}
+console.log([-1, 0, 2, -2, 1].twoSum());

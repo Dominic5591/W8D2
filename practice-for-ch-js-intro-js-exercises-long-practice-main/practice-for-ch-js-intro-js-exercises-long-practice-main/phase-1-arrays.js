4ed452bfb9a85464a3420a83352b5172ff363d35
Dominic <domcashiola@gmail.com>
2023-12-05 14:20:17 -0500

    Finished phase 1 problem Uniq

--- practice-for-ch-js-intro-js-exercises-long-practice-main/practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
@@ -1,1 +1,18 @@
 // Your code here
+
+
+Array.prototype.uniq = function() {
+  let uniqueArr = [];
+
+
+  for (let i = 0; i < this.length; i++) {
+    if (!uniqueArr.includes(this[i])) {
+      uniqueArr.push(this[i]);
+    };
+
+  };
+  return uniqueArr;
+};
+
+
+console.log([1,2,2,3,3,3].uniq())

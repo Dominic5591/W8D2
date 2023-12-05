c2cf450400d4ed7efe0446ee20bff367931758a0
Yaqi Fang <cfang322@gmail.com>
2023-12-05 16:19:10 -0500

    Finish phase 2 myEach problem

--- practice-for-ch-js-intro-js-exercises-long-practice-main/practice-for-ch-js-intro-js-exercises-long-practice-main/phase-2-enumerable.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/practice-for-ch-js-intro-js-exercises-long-practice-main/phase-2-enumerable.js
@@ -1,1 +1,13 @@
 // Your code here
+
+Array.prototype.myEach = function (callback) {
+    let arr = [];
+    for (let i = 0; i < this.length; i++){
+        arr.push(callback(this[i]))
+    }
+    return arr
+}
+
+console.log([1,2,3,4,5].myEach(function(ele){
+    console.log(ele)}));
+
